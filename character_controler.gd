56f1761306cd29833ac2a9992719c644868cb95a
SCRIPTERBLOX <scriptoblox@gmail.com>
2025-04-11 13:40:42 +0100

    Reformated the file a bit

--- character_controler.gd
+++ character_controler.gd
@@ -2,11 +2,7 @@ extends MeshInstance3D
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_up"):
-		position.z -= 1 * delta * 5
-	elif Input.is_action_pressed("ui_down"):
-		position.z += 1 * delta * 5
-	elif Input.is_action_pressed("ui_left"):
-		position.x -= 1 * delta * 5
-	elif Input.is_action_pressed("ui_right"):
-		position.x += 1 * delta * 5
+	if Input.is_action_pressed("ui_up"): position.z -= 1 * delta * 5
+	elif Input.is_action_pressed("ui_down"): position.z += 1 * delta * 5
+	elif Input.is_action_pressed("ui_left"): position.x -= 1 * delta * 5
+	elif Input.is_action_pressed("ui_right"): position.x += 1 * delta * 5
