65a417f53bf92be24b5a23b55e0545c19a87eca1
SCRIPTERBLOX <scriptoblox@gmail.com>
2025-04-11 11:49:08 +0100

    Added character controlling

---
+++ character_controler.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_up"):
+		position.z -= 1 * delta * 5
+	elif Input.is_action_pressed("ui_down"):
+		position.z += 1 * delta * 5
+	elif Input.is_action_pressed("ui_left"):
+		position.x -= 1 * delta * 5
+	elif Input.is_action_pressed("ui_right"):
+		position.x += 1 * delta * 5
